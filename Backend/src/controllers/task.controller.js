89b447f53c3c72a658ae24721ef8606291ef4c8f
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-02-26 20:21:32 +0530

    feat: update task endpoint

--- Backend/src/controllers/task.controller.js
+++ Backend/src/controllers/task.controller.js
@@ -6,8 +6,8 @@ import { Task } from "../models/task.model.js";
 const createNewTask = AsyncHandler(async (req, res) => {
   const { name, description, dueDate, isCompleted, isFavorite } = req.body;
 
-  if(name === "" || name === null){
-    throw new ApiError(400,"Task Name Field is Required!",{});
+  if (name === "" || name === null) {
+    throw new ApiError(400, "Task Name Field is Required!", {});
   }
 
   const newTask = await Task.create({
@@ -16,34 +16,56 @@ const createNewTask = AsyncHandler(async (req, res) => {
     dueDate: dueDate || "",
     isCompleted: isCompleted || false,
     isFavorite: isFavorite || false,
-  });  
+  });
 
   return res
     .status(201)
     .json(new ApiResponse(201, "Successfully created task", newTask));
 });
 
-const getAllTask = AsyncHandler(async (req,res)=>{
-
-  const tasks = await Task.find();  
+const getAllTask = AsyncHandler(async (req, res) => {
+  const tasks = await Task.find();
 
   return res
     .status(200)
     .json(new ApiResponse(200, "All Tasks Fetched!", tasks));
 });
 
-const getTask = AsyncHandler(async(req,res)=>{
-  const {taskId} = req.params;  
+const getTask = AsyncHandler(async (req, res) => {
+  const { taskId } = req.params;
 
-  if(!taskId){
-    throw new ApiError(400,"Task Id missing!",{});
-  };
+  if (!taskId) {
+    throw new ApiError(400, "Task Id missing!", {});
+  }
 
   const task = await Task.findById(taskId);
 
-  return res
-    .status(200)
-    .json(new ApiResponse(200, "Task Fetched!", task));
+  return res.status(200).json(new ApiResponse(200, "Task Fetched!", task));
 });
 
-export { createNewTask,getAllTask,getTask };
+const updateTask = AsyncHandler(async (req, res) => {
+  const { taskId, name, description, dueDate, isCompleted, isFavorite } =
+    req.body;
+  if (!taskId) {
+    throw new ApiError(400, "Task Id missing!", {});
+  }
+
+  const taskIdVerified = await Task.findById(taskId);
+  if (!taskIdVerified) {
+    throw new ApiError(403, "Task Id is Invalid!", {});
+  }
+
+  const updatedTask = await Task.findByIdAndUpdate(taskId, {
+    name: name || taskIdVerified.name,
+    description: description || taskIdVerified.description,
+    dueDate: dueDate || taskIdVerified.dueDate,
+    isCompleted: isCompleted || taskIdVerified.isCompleted,
+    isFavorite: isFavorite || taskIdVerified.isFavorite,
+  });
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, "Task Updated", updateTask));
+});
+
+export { createNewTask, getAllTask, getTask ,updateTask};
--- Backend/src/routes/task.route.js
+++ Backend/src/routes/task.route.js
@@ -1,8 +1,9 @@
 import {Router} from "express";
-import { createNewTask, getAllTask,getTask } from "../controllers/task.controller.js";
+import { createNewTask, getAllTask,getTask, updateTask } from "../controllers/task.controller.js";
 
 export const taskRouter = Router();
 
 taskRouter.post('/',createNewTask);
 taskRouter.get('/',getAllTask);
 taskRouter.get('/:taskId',getTask);
+taskRouter.put('/',updateTask);
