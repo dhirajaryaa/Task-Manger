798e9f42f87bc2c37604f1a58598fff06166f495
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-02-26 19:55:43 +0530

    fix: create task req task name fixed

--- Backend/src/controllers/task.controller.js
+++ Backend/src/controllers/task.controller.js
@@ -6,12 +6,10 @@ import { Task } from "../models/task.model.js";
 const createNewTask = AsyncHandler(async (req, res) => {
   const { name, description, dueDate, isCompleted, isFavorite } = req.body;
 
-  const missedField = ["name", "description"].find((field) => !req.body[field]);
-
-  if (missedField) {
-    throw new ApiError(400, `${missedField} is Required!`);
+  if(name === "" || name === null){
+    throw new ApiError(400,"Task Name Field is Required!",{});
   }
-
+  
   const newTask = await Task.create({
     name,
     description,
