479a76dce1e2bb03ee2385786229afab880ed166
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-02-28 19:33:39 +0530

    feat: frontend setup with tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <main className="bg-gray-950 w-full h-screen text-gray-50">
+      <div className="container mx-auto p-5">
+        <h1 className="md:text-6xl sm:text-4xl text-2l font-bold capitalize ">
+          Hello 👋, <span className="block">Dhiraj Arya</span>
+        </h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
